80a3d57f0dc8044e2516f02554c3cc10a8364252
gamutegamute <v25086vv@aitech.ac.jp>
2026-05-21 21:02:53 +0900

    api

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,5 +1,4 @@
 // lib/constants/api_constants.dart
 class ApiConstants {
-  static const String googleMapsApiKey =
-      'AIzaSyD2_CzfbYZi-TRZiATCUMCqeSqo-jYm4n4';
+  static const String googleMapsApiKey = '';
 }
